dff502b83289e0589512f3e98df93cf040961c7e
Karlo Espiritu <karloespiritu@gmail.com>
2016-07-05 11:30:31 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+#Computer Science Core Fundamentals in JavaScript
+
+Basic fundamental data structures and algorithms written in ES6.
+
+##Data Structures
+
+* Binary Trees
+* Doubly Linked Lists
+* HashTables
+* MaxHeaps
+* Queues
+* Singly Linked Lists
+* Stacks
+* Tries
+
+##Sorting Algorithms
+
+* Binary Search
+* Merge Sort
+* Quick Sort
---
+++ data-structures/DoublyLinkedList.js
@@ -0,0 +1,93 @@
+/*
+* @Author: karlo
+* @Date:   2016-07-05 02:27:52
+* @Last Modified by:   karlo
+* @Last Modified time: 2016-07-05 11:25:46
+*/
+
+'use strict';
+
+class LinkedNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insert(data) {
+    let node = new LinkedNode(data);
+
+    if (!this.head) {
+        this.head = this.tail = node
+    }
+    else {
+        this.tail.next = node
+        node.prev      = this.tail
+        this.tail      = node
+    }
+
+    return node
+  }
+
+  remove(data) {
+
+    if (!this.head.next) {
+      this.head = this.tail = null
+    }
+    else {
+      let curr = this.head
+      while (curr) {
+        if (curr.data === data) {
+            curr.prev.next = curr.next
+            curr.next.prev = curr.prev
+            curr = null
+        }
+        else {
+            curr = curr.next
+        }
+      }
+    }
+  }
+
+  find(data) {
+
+        let curr = this.head
+
+        while (curr) {
+            if (curr.data === data) {
+                return curr
+            }
+            curr = curr.next
+        }
+
+  }
+
+  print() {
+    let curr = this.head;
+
+    while (curr) {
+        console.log(curr);
+        curr = curr.next;
+    }
+  }
+}
+
+    var myList = new DoublyLinkedList();
+
+    myList.insert('L');
+    myList.insert('M');
+    myList.insert('N');
+    myList.insert('O');
+    myList.insert('P');
+
+    myList.remove('M');
+    myList.find('L');
+    myList.print();
+
